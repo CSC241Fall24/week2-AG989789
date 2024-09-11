cb8732a95d210c801ed2e48685108db60e21d3c7
AG989789 <126360569+AG989789@users.noreply.github.com>
2024-09-11 11:44:42 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -8,7 +8,7 @@ public class Q1WeirdFunction {
    }
    
    public static int fIterative(int n) {
-      while(n < 3)
+      while(!(n < 3))
          n = (n - 1) + 2 * (n - 2) + 3 * (n - 3);
       return n;
    }
