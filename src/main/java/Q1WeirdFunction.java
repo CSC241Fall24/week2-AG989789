997611c3127c29775c5e73eb3a69b9e20d52c4a9
AG989789 <126360569+AG989789@users.noreply.github.com>
2024-09-11 11:42:25 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -4,12 +4,12 @@ public class Q1WeirdFunction {
 
    public static int fRecursive(int n) {
       if(n < 3) return n;
-      return fRecursive(n - 1) + fRecursive(n - 2) + fRecursive(n - 3);
+      return fRecursive(n - 1) + 2 * fRecursive(n - 2) + 3 * fRecursive(n - 3);
    }
    
    public static int fIterative(int n) {
       while(n < 3)
-         n = (n - 1) + (n - 2) + (n - 3);
+         n = (n - 1) + 2 * (n - 2) + 3 * (n - 3);
       return n;
    }
 }
