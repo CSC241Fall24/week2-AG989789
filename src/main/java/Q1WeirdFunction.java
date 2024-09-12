a19da3db59b661d4edb97021f6414cf378ed9fcd
AG989789 <126360569+AG989789@users.noreply.github.com>
2024-09-12 15:58:15 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -8,9 +8,17 @@ public class Q1WeirdFunction {
    }
    
    public static int fIterative(int n) {
-      while(n <= 3) {
-         n = (n - 1) + 2 * (n - 2) + 3 * (n - 3);
-      }
-      return n;
+    if (n < 3) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + 2 * dp[i - 2] + 3 * dp[i - 3];
+        }
+        return dp[n];
    }
 }
